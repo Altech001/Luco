a4fac05d5c19d9d4620eea3ab43ba27602e68188
Pico <picopay01@gmail.com>
2025-09-29 10:20:33 +0000

    BORDER RADUIS TO 4PX

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -77,8 +77,8 @@ export default {
       },
       borderRadius: {
         lg: 'var(--radius)',
-        md: 'calc(var(--radius))',
-        sm: 'calc(var(--radius))',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
         'accordion-down': {
