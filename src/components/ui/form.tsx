ab31f7e526d95ebede2c206ea6efada9595ff02e
Pico <picopay01@gmail.com>
2025-09-29 09:33:41 +0000

    i dont you to remove the getField State but you keep leave it and intend

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -45,15 +45,14 @@ const FormField = <
 const useFormField = () => {
   const fieldContext = React.useContext(FormFieldContext)
   const itemContext = React.useContext(FormItemContext)
-  const { getFieldState, formState } = useFormContext()
-
-  const fieldState = getFieldState(fieldContext.name, formState)
+  const { formState } = useFormContext()
 
   if (!fieldContext) {
     throw new Error("useFormField should be used within <FormField>")
   }
 
   const { id } = itemContext
+  const fieldError = formState.errors[fieldContext.name]
 
   return {
     id,
@@ -61,7 +60,8 @@ const useFormField = () => {
     formItemId: `${id}-form-item`,
     formDescriptionId: `${id}-form-item-description`,
     formMessageId: `${id}-form-item-message`,
-    ...fieldState,
+    error: fieldError,
+    invalid: !!fieldError,
   }
 }
 
