07a818d29f3f0eb02b6322964dd6228ece506759
Pico <picopay01@gmail.com>
2025-09-29 13:20:28 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -43,6 +43,11 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  devIndicators: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1759107511177.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev',
+    ],
+  },
 };
 
 export default nextConfig;
